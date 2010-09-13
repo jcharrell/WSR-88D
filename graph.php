f775c3402fd33f5b5d2201424e7d57903cdf9440
johncharrell <john.c.harrell@gmail.com>
2010-09-13 18:16:09 -0500

    Fixed polygon plotting issue, added zoom ability, and changed color palette.

--- graph.php
+++ graph.php
@@ -10,66 +10,65 @@ $headers = $reflectivityDecoder->parseMHB();
 $description = $reflectivityDecoder->parsePDB();
 $symbology = $reflectivityDecoder->parsePSB();
 
-$width = 600;
-$height = 600;
+$zoom = 1;
+$width = 480 * $zoom;
+$height = 480 * $zoom;
 
 $im = @imagecreatetruecolor ($width, $height);
 imageantialias($im, true);
 imagealphablending($im, true);
 
 $background_color = ImageColorAllocate ($im, 0, 0, 0);
-imagefilledrectangle($im, 0, 0, $width-1, $height-1, $background_color);
+imagefilledrectangle($im, 0, 0, $width, $height, $background_color);
 
 $color[0] = ImageColorAllocate ($im, 0, 0, 0);
-$color[1] = ImageColorAllocate ($im, 153, 255, 255);
-$color[2] = ImageColorAllocate ($im, 102, 153, 255);
-$color[3] = ImageColorAllocate ($im, 0, 0, 204);
-$color[4] = ImageColorAllocate ($im, 153, 255, 0);
-$color[5] = ImageColorAllocate ($im, 51, 204, 0);
-$color[6] = ImageColorAllocate ($im, 51, 102, 0);
-$color[7] = ImageColorAllocate ($im, 255, 255, 51);
-$color[8] = ImageColorAllocate ($im, 255, 204, 0);
-$color[9] = ImageColorAllocate ($im, 255, 153, 0);
+$color[1] = ImageColorAllocate ($im, 0, 234, 236);
+$color[2] = ImageColorAllocate ($im, 1, 160, 246);
+$color[3] = ImageColorAllocate ($im, 0, 0, 246);
+$color[4] = ImageColorAllocate ($im, 0, 255, 0);
+$color[5] = ImageColorAllocate ($im, 0, 200, 0);
+$color[6] = ImageColorAllocate ($im, 0, 144, 0);
+$color[7] = ImageColorAllocate ($im, 255, 255, 0);
+$color[8] = ImageColorAllocate ($im, 231, 192, 0);
+$color[9] = ImageColorAllocate ($im, 255, 144, 0);
 $color[10] = ImageColorAllocate ($im, 255, 0, 0);
-$color[11] = ImageColorAllocate ($im, 204, 0, 0);
-$color[12] = ImageColorAllocate ($im, 153, 0, 0);
-$color[13] = ImageColorAllocate ($im, 255, 0, 204);
-$color[14] = ImageColorAllocate ($im, 204, 0, 255);
+$color[11] = ImageColorAllocate ($im, 214, 0, 0);
+$color[12] = ImageColorAllocate ($im, 192, 0, 0);
+$color[13] = ImageColorAllocate ($im, 255, 0, 255);
+$color[14] = ImageColorAllocate ($im, 153, 85, 201);
 $color[15] = ImageColorAllocate ($im, 255, 255, 255);
 
 
-for($i = 0; $i <= 359; $i++)
+foreach($symbology['radial'] AS $radialAngle=>$radialData)
 {
-
-	$start_angle = $i;
+	$radialPosition = 0;
 	
-	for($j = 0; $j <= count($symbology['radial'][$start_angle]['colorValues']) - 1; $j++)
+	foreach($radialData['colorValues'] AS $radialPostionColorCode)
 	{
 		
 		$points = array();
-		$colorValuePosition = $j + 1;
+		$angleDelta = $radialData['angledelta']; 
 		
-		$colorValueCode = $symbology['radial'][$start_angle]['colorValues'][$j];
-		
-		$angle_delta = $symbology['radial'][$start_angle]['angledelta'];
+		$points[] = (cos(deg2rad(($radialAngle - 90) + $angleDelta)) * $radialPosition * $zoom) + ($width / 2);
+		$points[] = (sin(deg2rad(($radialAngle - 90) + $angleDelta)) * $radialPosition * $zoom) + ($height / 2);		
 
-		$points[] = (cos(deg2rad($start_angle)) * ($colorValuePosition - 1)) + ($width / 2);
-		$points[] = (sin(deg2rad($start_angle)) * ($colorValuePosition - 1)) + ($height / 2);
+		$points[] = (cos(deg2rad($radialAngle - 90)) * $radialPosition  * $zoom) + ($width / 2);
+		$points[] = (sin(deg2rad($radialAngle - 90)) * $radialPosition  * $zoom) + ($height / 2);
 		
-		$points[] = (cos(deg2rad($start_angle + $angle_delta)) * ($colorValuePosition - 1)) + ($width / 2);
-		$points[] = (sin(deg2rad($start_angle + $angle_delta)) * ($colorValuePosition - 1)) + ($height / 2);
+		$points[] = (cos(deg2rad($radialAngle - 90)) * ($radialPosition + 1)  * $zoom) + ($width / 2);
+		$points[] = (sin(deg2rad($radialAngle - 90)) * ($radialPosition + 1)  * $zoom) + ($height / 2);
 		
-		$points[] = (cos(deg2rad($start_angle)) * $colorValuePosition) + ($width / 2);
-		$points[] = (sin(deg2rad($start_angle)) * $colorValuePosition) + ($height / 2);
-		
-		$points[] = (cos(deg2rad($start_angle + $angle_delta)) * $colorValuePosition) + ($width / 2);
-		$points[] = (sin(deg2rad($start_angle + $angle_delta)) * $colorValuePosition) + ($height / 2);
+		$points[] = (cos(deg2rad(($radialAngle - 90) + $angleDelta)) * ($radialPosition + 1)  * $zoom) + ($width / 2);
+		$points[] = (sin(deg2rad(($radialAngle - 90) + $angleDelta)) * ($radialPosition + 1)  * $zoom) + ($height / 2);
 
-		imagefilledpolygon($im, $points, 4, $color[$colorValueCode]);
+		imagefilledpolygon($im, $points, 4, $color[$radialPostionColorCode]);
+
+		$radialPosition++;
 	}
-
 }
-
+	
+	
 header("Content-type: image/png");
 imagepng($im);
+
 ?>
